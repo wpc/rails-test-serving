6b3987d01425de11da4317a9127f6bf379405c7f
Roman Le Negrate <roman.lenegrate@gmail.com>
2008-11-30 10:27:55 +0100

    moved RailsTestServing singleton methods to a Bootstrap module

--- lib/rails_test_serving.rb
+++ lib/rails_test_serving.rb
@@ -11,66 +11,12 @@ module RailsTestServing
   class ServerUnavailable < StandardError
   end
   
-  SOCKET_PATH = ['tmp', 'sockets', 'test_server.sock']
-  
-  class << self
-    def boot(argv=ARGV)
-      if argv.delete('--serve')
-        start_server
-      elsif !argv.delete('--local')
-        Client.run_tests
-      end
-    end
-    
-    def service_uri
-      @service_uri ||= begin
-        # Determine RAILS_ROOT
-        root, max_depth = Pathname('.'), Pathname.pwd.expand_path.to_s.split(File::SEPARATOR).size
-        until root.join('config', 'boot.rb').file?
-          root = root.parent
-          if root.to_s.split(File::SEPARATOR).size >= max_depth
-            raise "RAILS_ROOT could not be determined"
-          end
-        end
-        root = root.cleanpath
-      
-        # Adjust load path
-        $: << root.to_s << root.join('test').to_s
-      
-        # Ensure socket directory exists
-        path = root.join(*SOCKET_PATH)
-        path.dirname.mkpath
-      
-        # URI
-        "drbunix:#{path}"
-      end
-    end
-    
-    def options
-      @options ||= begin
-        options = $test_server_options || {}
-        options[:reload] ||= []
-        options
-      end
-    end
-    
-    def active?
-      @active
-    end
-    
-  private
-  
-    def start_server
-      @active = true
-      Server.start
-    ensure
-      @active = false
-    end
-  end
-  
+  autoload :Bootstrap,  'rails_test_serving/bootstrap'
   autoload :Server,     'rails_test_serving/server'
   autoload :Client,     'rails_test_serving/client'
   autoload :Cleaner,    'rails_test_serving/cleaner'
   autoload :ConstantManagement, 'rails_test_serving/constant_management'
   autoload :Utilities,  'rails_test_serving/utilities'
-end unless defined? RailsTestServing
+  
+  extend Bootstrap
+end
--- test/rails_test_serving/bootstrap_test.rb
+++ test/rails_test_serving/bootstrap_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
-class RailsTestServingTest < Test::Unit::TestCase
-  include RailsTestServing
+class RailsTestServing::BootstrapTest < Test::Unit::TestCase
+  def setup
+    @boot = Object.new.extend RailsTestServing::Bootstrap
+  end
   
-# class
-
   def test_service_uri
     # RAILS_ROOT is the current directory
     setup_service_uri_test do
       FileTest.expects(:file?).with("config/boot.rb").returns true
       FileUtils.expects(:mkpath).with("tmp/sockets")
-      assert_equal "drbunix:tmp/sockets/test_server.sock", RailsTestServing.service_uri
+      assert_equal "drbunix:tmp/sockets/test_server.sock", @boot.service_uri
     end
-    
+
     # RAILS_ROOT is in the parent directory
     setup_service_uri_test do
       FileTest.stubs(:file?).with("config/boot.rb").returns false
       FileTest.stubs(:file?).with("../config/boot.rb").returns true
       FileUtils.expects(:mkpath).with("../tmp/sockets")
-      assert_equal "drbunix:../tmp/sockets/test_server.sock", RailsTestServing.service_uri
+      assert_equal "drbunix:../tmp/sockets/test_server.sock", @boot.service_uri
     end
-    
+
     # RAILS_ROOT cannot be determined
     setup_service_uri_test do
       Pathname.stubs(:pwd).returns(Pathname("/foo/bar"))
@@ -29,40 +29,40 @@ class RailsTestServingTest < Test::Unit::TestCase
       FileTest.expects(:file?).with("../../config/boot.rb").returns false
       FileTest.expects(:file?).with("../../../config/boot.rb").never
       FileUtils.expects(:mkpath).never
-      assert_raise(RuntimeError) { RailsTestServing.service_uri }
+      assert_raise(RuntimeError) { @boot.service_uri }
     end
   end
 
   def test_boot
     argv = []
-    Client.expects(:run_tests)
-    RailsTestServing.boot(argv)
-    
+    RailsTestServing::Client.expects(:run_tests)
+    @boot.boot(argv)
+
     argv = ['--local']
-    Client.expects(:run_tests).never
-    RailsTestServing.boot(argv)
+    RailsTestServing::Client.expects(:run_tests).never
+    @boot.boot(argv)
     assert_equal [], argv
-    
+
     argv = ["--serve"]
-    RailsTestServing.expects(:start_server)
-    RailsTestServing.boot(argv)
+    @boot.expects(:start_server)
+    @boot.boot(argv)
     assert_equal [], argv
   end
-  
+
   def test_options
-    RailsTestServing.instance_variable_set("@options", nil)
+    @boot.instance_variable_set("@options", nil)
     $test_server_options = nil
-    assert_equal({:reload => []}, RailsTestServing.options)
-    
-    RailsTestServing.instance_variable_set("@options", nil)
+    assert_equal({:reload => []}, @boot.options)
+
+    @boot.instance_variable_set("@options", nil)
     $test_server_options = {:foo => :bar}
-    assert_equal({:foo => :bar, :reload => []}, RailsTestServing.options)
-    
-    RailsTestServing.instance_variable_set("@options", nil)
+    assert_equal({:foo => :bar, :reload => []}, @boot.options)
+
+    @boot.instance_variable_set("@options", nil)
     $test_server_options = {:foo => :bar, :reload => [//]}
-    assert_equal({:foo => :bar, :reload => [//]}, RailsTestServing.options)
+    assert_equal({:foo => :bar, :reload => [//]}, @boot.options)
   end
-  
+
 private
 
   def setup_service_uri_test
@@ -70,7 +70,7 @@ private
     begin
       return yield
     ensure
-      RailsTestServing.instance_variable_set("@service_uri", nil)
+      @boot.instance_variable_set("@service_uri", nil)
       $:.replace(old_load_path)
     end
   end
